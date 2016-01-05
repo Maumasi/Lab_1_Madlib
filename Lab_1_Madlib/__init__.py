fa68089d08afb90dbfa5ee3c6bc3e80751dd8230
maumasi <liu.maumasi@gmail.com>
2016-01-05 15:43:35 -0500

    added requirements in a comment

---
+++ Lab_1_Madlib/__init__.py
@@ -0,0 +1,24 @@
+# Create your own mad lib  that collects (at least) the following information:
+#
+# At least 2 strings
+# At least 2 numbers
+# And includes the following elements:
+#
+# One list/array
+# One conditional statement with at least one logical operator
+# One function
+#   Must return a value
+#   Must have and use parameters
+# One loop
+
+
+# And meet the following requirements:
+#
+# The mad lib's code must be well commented.
+# Comments should explain code, not just label.
+# All elements should have a purpose within the greater madlib.
+# (In other words you can't create an array or function that isn't used)
+# You must make required number meaningful commits to this file in your repository
+# Any errors will earn an automatic ZERO on this assignment.
+# Your mad lib should print out the entire story to the console.
+
