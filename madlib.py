9ff5faecac78d2715e4d083dedcdc9c3563ce3f8
maumasi <liu.maumasi@gmail.com>
2016-01-05 22:26:28 -0500

    made new file
    I made a new file and logic code is working

--- madlib.py
+++ madlib.py
@@ -1,11 +1,9 @@
-# Create your own mad lib  that collects (at least) the following information:
-#
 # At least 2 strings
 # At least 2 numbers
 # And includes the following elements:
 #
 # One list/array
-# One conditional statement with at least one logical operator
+# One if statement with at least one logical operator
 # One function
 #   Must return a value
 #   Must have and use parameters
@@ -22,4 +20,32 @@
 # Any errors will earn an automatic ZERO on this assignment.
 # Your mad lib should print out the entire story to the console.
 
-def 
+# Mad Lib:
+# There was a place were NOUN changed the way people ate their food.
+# One day NAME ate NUMBER1 FOOD that made NAME very ill for NUMBER2 days.
+# NAME , has never eaten FOOD again.
+
+userMadLibList = []
+
+print "Give me a noun."
+userMadLibList.append(raw_input())
+
+while len(userMadLibList) != 5:
+    for i in range(len(userMadLibList)):
+        if len(userMadLibList) == 0:
+            print "You didn't answer the first question!"
+            userMadLibList.append(raw_input())
+        elif len(userMadLibList) == 1:
+            print "What's your name?"
+            userMadLibList.append(raw_input())
+        elif len(userMadLibList) == 2:
+            print "Give me a number."
+            userMadLibList.append(raw_input())
+        elif len(userMadLibList) == 3:
+            print "What's your favorite food?"
+            userMadLibList.append(raw_input())
+        elif len(userMadLibList) == 4:
+            print "Give me another number."
+            userMadLibList.append(raw_input())
+
+print userMadLibList
